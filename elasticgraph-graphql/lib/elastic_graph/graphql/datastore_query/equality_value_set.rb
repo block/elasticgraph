7690cb1fcdde9902d3ac5e865226bfdcc4a3a8d9
Marc Daniels <marc.daniels@toasttab.com>
2026-06-04 21:23:04 -0400

    Extract EqualityValueSet as a shared set abstraction for filter value extraction.
    Introduces `EqualityValueSet`, extracted from `RoutingValueSet`, to serve as a
    reusable inclusive/exclusive set for use with `FilterValueSetExtractor`. Adds
    `FilterValueSetExtractor.for_equality` as a factory method to reduce boilerplate
    for callers.

---
+++ elasticgraph-graphql/lib/elastic_graph/graphql/datastore_query/equality_value_set.rb
@@ -0,0 +1,113 @@
+# Copyright 2024 - 2026 Block, Inc.
+#
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+#
+# frozen_string_literal: true
+
+module ElasticGraph
+  class GraphQL
+    class DatastoreQuery
+      # A set that can represent either a specific list of values or all values except a specific
+      # list, with support for common set operations (union, intersection, negation).
+      class EqualityValueSet < Data.define(:type, :values)
+        # `Data.define` provides the following methods:
+        # @dynamic initialize, type, values, with
+        def self.of(values)
+          new(:inclusive, values.to_set)
+        end
+
+        def self.of_all_except(values)
+          new(:exclusive, values.to_set)
+        end
+
+        ALL = of_all_except([])
+        EMPTY = of([])
+
+        def include?(value)
+          inclusive? ? values.include?(value) : !values.include?(value)
+        end
+
+        def intersection(other)
+          if inclusive? && other.inclusive?
+            # Since both sets are inclusive, we can just delegate to `Set#intersection` here.
+            EqualityValueSet.of(values.intersection(other.values))
+          elsif exclusive? && other.exclusive?
+            # Since both sets are exclusive, we need to return an exclusive set of the union of the
+            # excluded values. For example, when dealing with positive integers:
+            #
+            #   s1 = EqualityValueSet.of_all_except([1, 2, 3]) # > 3
+            #   s2 = EqualityValueSet.of_all_except([3, 4, 5]) # 1, 2, > 5
+            #
+            #   s3 = s1.intersection(s2)
+            #
+            # Here s3 would be all values > 5 (the same as `EqualityValueSet.of_all_except([1, 2, 3, 4, 5])`)
+            EqualityValueSet.of_all_except(values.union(other.values))
+          else
+            # Since one set is inclusive and one set is exclusive, we need to return an inclusive set of
+            # `included_values - excluded_values`. For example, when dealing with positive integers:
+            #
+            #   s1 = EqualityValueSet.of([1, 2, 3]) # 1, 2, 3
+            #   s2 = EqualityValueSet.of_all_except([3, 4, 5]) # 1, 2, > 5
+            #
+            #   s3 = s1.intersection(s2)
+            #
+            # Here s3 would be just `1, 2`.
+            included, excluded = inclusive? ? [values, other.values] : [other.values, values]
+            EqualityValueSet.of(included - excluded)
+          end
+        end
+
+        def union(other)
+          if inclusive? && other.inclusive?
+            # Since both sets are inclusive, we can just delegate to `Set#union` here.
+            EqualityValueSet.of(values.union(other.values))
+          elsif exclusive? && other.exclusive?
+            # Since both sets are exclusive, we need to return an exclusive set of the intersection of the
+            # excluded values. For example, when dealing with positive integers:
+            #
+            #   s1 = EqualityValueSet.of_all_except([1, 2, 3]) # > 3
+            #   s2 = EqualityValueSet.of_all_except([3, 4, 5]) # 1, 2, > 5
+            #
+            #   s3 = s1.union(s2)
+            #
+            # Here s3 would be all 1, 2, > 3 (the same as `EqualityValueSet.of_all_except([3])`)
+            EqualityValueSet.of_all_except(values.intersection(other.values))
+          elsif inclusive?
+            # Since one set is inclusive and one set is exclusive, we need to return an exclusive set of
+            # `excluded_values - included_values`. For example, when dealing with positive integers:
+            #
+            #   s1 = EqualityValueSet.of([1, 2, 3]) # 1, 2, 3
+            #   s2 = EqualityValueSet.of_all_except([3, 4, 5]) # 1, 2, > 5
+            #
+            #   s3 = s1.union(s2)
+            #
+            # Here s3 would be 1, 2, 3, > 5 (the same as `EqualityValueSet.of_all_except([4, 5])`)
+            EqualityValueSet.of_all_except(other.values - values)
+          else
+            # s1 is exclusive, s2 is inclusive: symmetric to the above.
+            EqualityValueSet.of_all_except(values - other.values)
+          end
+        end
+
+        def negate
+          with(type: INVERTED_TYPES.fetch(type))
+        end
+
+        INVERTED_TYPES = {inclusive: :exclusive, exclusive: :inclusive}
+
+        def inclusive?
+          type == :inclusive
+        end
+
+        def exclusive?
+          type == :exclusive
+        end
+      end
+
+      # Steep is complaining that it can't find some `DatastoreQuery` methods but they are not in this file...
+      # @dynamic shard_routing_values, effective_size, merge_with, search_index_expression, with, to_datastore_msearch_header_and_body
+    end
+  end
+end
