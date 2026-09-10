677214ef6fba3011a2945b3d339c7c54609bf532
Josh Wilson <joshuaw@squareup.com>
2026-09-10 12:43:06 -0500

    Keep JSON version selection in its adapter

--- elasticgraph-support/lib/elastic_graph/constants.rb
+++ elasticgraph-support/lib/elastic_graph/constants.rb
@@ -137,10 +137,6 @@ module ElasticGraph
   # @private
   JSON_SCHEMA_VERSION_KEY = "json_schema_version"
 
-  # Name for field in indexing events that identifies the schema artifact version to use.
-  # @private
-  SCHEMA_VERSION_KEY = "schema_version"
-
   # The key that identifies the ingestion format of a decoded event.
   # @return [String]
   INGESTION_FORMAT_KEY = "ingestion_format"
